a8e03a20605149611518870e44867235e7bbc209
OraonShivam21 <oraonshivam.0302@gmail.com>
2024-01-13 15:25:50 +0530

    created swagger ui for User

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,6 +6,71 @@ require("dotenv").config();
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *        User:
+ *            type: object
+ *            properties:
+ *                id:
+ *                    type: string
+ *                    description: The auto generated _id for each user
+ *                username:
+ *                    type: string
+ *                    description: The username of the user
+ *                email:
+ *                    type: string
+ *                    description: The email of the user
+ *                password:
+ *                    type: string
+ *                    description: The hashed password of the user (hashed using bcrypt)
+ */
+
+/**
+ * @swagger
+ * /user/register:
+ *  post:
+ *    summary: To register a new user in the database
+ *    tags: [User]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              username:
+ *                type: string
+ *                example: "your_username"
+ *                required: true
+ *              email:
+ *                type: string
+ *                example: "your_email"
+ *                required: true
+ *              password:
+ *                type: string
+ *                example: "your_password"
+ *                required: true
+ *    responses:
+ *      201:
+ *        description: Register a new user using username, email, password
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              example: { message: "New user has been registerd" }
+ *      400:
+ *        description: The error message occuring if there is any problem
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              example: { error: "Some kind of error message" }
+ *      500:
+ *        description: Some server error message
+ */
+
 router.post("/register", (req, res) => {
   const { username, email, password } = req.body;
   try {
@@ -20,6 +85,46 @@ router.post("/register", (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /user/login:
+ *  post:
+ *    summary: To login the user in the application
+ *    tags: [User]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              email:
+ *                type: string
+ *                example: "example@gmail.com"
+ *                required: true
+ *              password:
+ *                type: string
+ *                example: "example#password"
+ *                required: true
+ *    responses:
+ *      200:
+ *        description: Logs in the user if user had registered before
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              example: { message: "Successfully logged in", accessToken: "access token" }
+ *      400:
+ *        description: The error message occuring if there is any problem
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              example: { error: "Some kind of error message" }
+ *      500:
+ *        description: Some server error message
+ */
+
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   try {
@@ -27,17 +132,12 @@ router.post("/login", async (req, res) => {
     if (!userFound) throw "Please register yourself first!";
     bcrypt.compare(password, userFound.password, (err, result) => {
       if (err) throw err;
-      if (result) {
-        const accessToken = jwt.sign(
-          { userID: userFound._id, username: userFound.username },
-          process.env.accessSecret
-        );
-        res
-          .status(200)
-          .json({ message: "Successfully logged in", accessToken });
-      } else {
-        res.status(200).json({ message: "Invalid credentials" });
-      }
+      if (!result) throw "Invalid credentials";
+      const accessToken = jwt.sign(
+        { userID: userFound._id, username: userFound.username },
+        process.env.accessSecret
+      );
+      res.status(200).json({ message: "Successfully logged in", accessToken });
     });
   } catch (error) {
     res.status(400).json({ error });
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors")
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 const connection = require("./connection");
 const userRoute = require("./routes/user.route");
 const noteRoute = require("./routes/note.route");
@@ -11,11 +13,36 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Notes Management System with User registration, login, and authentication",
+      version: "1.0.0",
+    },
+    servers: [
+      { 
+        url: "http://localhost:3000", 
+        description: "Localhost link with port:3000" 
+      },
+      {
+        url: "https://notes-taking-web-app.onrender.com",
+        description: "Deployed link on render.com"
+      }
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const openAPISpec = swaggerJSDoc(options);
+
+app.use("/apidocs", swaggerUI.serve, swaggerUI.setup(openAPISpec));
+
 app.use("/user", userRoute);
 app.use("/note", noteRoute);
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Welcome to the notes taking api" });
+  res.status(200).json({ message: "Welcome to the backend api" });
 });
 
 app.listen(port, async () => {
